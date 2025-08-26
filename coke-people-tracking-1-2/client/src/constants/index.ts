f92f73b163132d8665eb5e104cd25607fbe4af44
Ajay Irkal <103778100+AjayIrkal23@users.noreply.github.com>
2025-08-26 16:21:49 +0530

    update

--- coke-people-tracking-1-2/client/src/constants/index.ts
+++ coke-people-tracking-1-2/client/src/constants/index.ts
@@ -1,6 +1,6 @@
 export const COLOR_PRIMARY = "#2c51b4";
-export const BATTERY_1_MAP_IMAGE = "/batrest.png";
-export const BATTERY_2_MAP_IMAGE = BATTERY_1_MAP_IMAGE;
+export const BATTERY_1_MAP_IMAGE = "/B.png";
+export const BATTERY_2_MAP_IMAGE = "/B-1.png";
 export const DEVICE_NORMALIZED_SIZE = 0.02;
 export const BEACON_DISPLAY_MINUTES = 5;
 export const CONNECT_POINT_ACTIVE_MINUTES = 60;
